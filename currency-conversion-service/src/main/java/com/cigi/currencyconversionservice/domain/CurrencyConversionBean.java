2cad96c34e1ea7b16f95a39c22bff57705688d6a
cozmus <cozmus@gmail.com>
2020-06-10 21:06:51 +0300

    currency-conversion-service is added.

---
+++ currency-conversion-service/src/main/java/com/cigi/currencyconversionservice/domain/CurrencyConversionBean.java
@@ -0,0 +1,20 @@
+package com.cigi.currencyconversionservice.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CurrencyConversionBean {
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+    private BigDecimal quantity;
+    private BigDecimal calculatedAmount;
+    private String port;
+}
