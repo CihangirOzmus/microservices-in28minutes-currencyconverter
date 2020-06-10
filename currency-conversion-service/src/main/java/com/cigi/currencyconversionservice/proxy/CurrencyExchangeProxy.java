4fffc6895749afed405d4055b2fea39969ca4a42
cozmus <cozmus@gmail.com>
2020-06-10 21:58:57 +0300

    Ribbon is added for load balancing.

--- currency-conversion-service/src/main/java/com/cigi/currencyconversionservice/proxy/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/cigi/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -1,11 +1,13 @@
 package com.cigi.currencyconversionservice.proxy;
 
 import com.cigi.currencyconversionservice.domain.CurrencyConversionBean;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+@FeignClient(name = "currency-exchange-service")
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     CurrencyConversionBean getExchangeValue(@PathVariable String from, @PathVariable String to);
