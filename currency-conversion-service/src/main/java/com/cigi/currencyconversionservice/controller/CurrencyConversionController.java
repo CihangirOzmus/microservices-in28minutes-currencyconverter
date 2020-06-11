5e438b55fb94e22db4610ae4dd2323b2bb005625
cozmus <cozmus@gmail.com>
2020-06-11 16:07:31 +0300

    Api gateway is used for requests.

--- currency-conversion-service/src/main/java/com/cigi/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/cigi/currencyconversionservice/controller/CurrencyConversionController.java
@@ -2,15 +2,11 @@ package com.cigi.currencyconversionservice.controller;
 
 import com.cigi.currencyconversionservice.domain.CurrencyConversionBean;
 import com.cigi.currencyconversionservice.proxy.CurrencyExchangeProxy;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class CurrencyConversionController {
@@ -19,18 +15,6 @@ public class CurrencyConversionController {
 
     public CurrencyConversionController(CurrencyExchangeProxy proxy) {
         this.proxy = proxy;
-    }
-
-    @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
-    public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("from", from);
-        uriVariables.put("to", to);
-
-        ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class, uriVariables);
-        CurrencyConversionBean response = responseEntity.getBody();
-
-        return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
     }
 
     @GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
