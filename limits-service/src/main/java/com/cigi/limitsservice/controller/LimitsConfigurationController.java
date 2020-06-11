4f5581556d4fcbeb179642f2313996bb83b9e1a1
cozmus <cozmus@gmail.com>
2020-06-11 15:41:13 +0300

    Zuul Gateway is added.

--- limits-service/src/main/java/com/cigi/limitsservice/controller/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/cigi/limitsservice/controller/LimitsConfigurationController.java
@@ -1,5 +1,6 @@
-package com.cigi.limitsservice;
+package com.cigi.limitsservice.controller;
 
+import com.cigi.limitsservice.Configuration;
 import com.cigi.limitsservice.bean.LimitConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
