1dce08595cbca015f09eeb27f4bf53585d670f6a
cozmus <cozmus@gmail.com>
2020-06-12 14:34:54 +0300

    Spring Cloud Sleuth is added for request tracing. All requests are traced with Sampler.

--- currency-conversion-service/src/main/java/com/cigi/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/cigi/currencyconversionservice/controller/CurrencyConversionController.java
@@ -2,6 +2,8 @@ package com.cigi.currencyconversionservice.controller;
 
 import com.cigi.currencyconversionservice.domain.CurrencyConversionBean;
 import com.cigi.currencyconversionservice.proxy.CurrencyExchangeProxy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,7 @@ import java.math.BigDecimal;
 
 @RestController
 public class CurrencyConversionController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private final CurrencyExchangeProxy proxy;
 
@@ -20,7 +23,7 @@ public class CurrencyConversionController {
     @GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
         CurrencyConversionBean response = proxy.getExchangeValue(from, to);
-
+        logger.info("{}", response);
         return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
     }
 }
--- currency-exchange-service/src/main/java/com/cigi/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/cigi/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.cigi.currencyexchangeservice.controller;
 
 import com.cigi.currencyexchangeservice.domain.ExchangeValue;
 import com.cigi.currencyexchangeservice.repository.ExchangeValueRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     private final ExchangeValueRepository exchangeValueRepository;
     private final Environment environment;
@@ -22,6 +25,7 @@ public class CurrencyExchangeController {
     public ExchangeValue getExchangeValue(@PathVariable String from, @PathVariable String to) {
         ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to);
         exchangeValue.setPort(environment.getProperty("local.server.port"));
+        logger.info("{}", exchangeValue);
         return exchangeValue;
     }
 }
